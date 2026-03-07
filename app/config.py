e3df3f553449333a3b8778c5e8bcd0e62fd1ddfa
ayush9492 <ayushbharatiya@gmail.com>
2026-03-06 23:49:01 -0600

    commit

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./bandit.db")
 N_ARMS = int(os.getenv("N_ARMS", 3))
-ALGORITHM = os.getenv("ALGORITHM", "thompson")   # thompson | epsilon_greedy | ucb
+ALGORITHM = os.getenv("ALGORITHM", "epsilon_greedy")   # thompson | epsilon_greedy | ucb
 EPSILON = float(os.getenv("EPSILON", 0.1))       # used by epsilon_greedy
 DEBUG = os.getenv("DEBUG", "True") == "True"
